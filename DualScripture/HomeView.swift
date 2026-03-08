713e58fce44b222f87d2daf1805bc20f4ba43dae
Michael <mknighter62@gmail.com>
2026-03-08 14:39:56 -0600

    Added in the logos for the books, and aded in navigationStack

--- DualScripture/HomeView.swift
+++ DualScripture/HomeView.swift
@@ -13,10 +13,117 @@ import SwiftSoup
 
 struct HomeView: View {
     var body: some View {
-        NavigationView {
-            NavigationLink(destination: VersesView()) {
-                Text("View verses")
-            }
+            NavigationStack {
+                ZStack {
+                    Color.black.ignoresSafeArea(.all)
+                    HStack {
+                        NavigationLink(destination: BOMChapterListView()) {
+                            VStack {
+                                VStack {
+                                    Rectangle()
+                                        .stroke(.gold, lineWidth: 1)
+                                        .frame(width: 80, height: 1)
+                                    Text("THE")
+                                        .font(.custom("Poppins-Regular", size: 13))
+                                        .foregroundStyle(.gold)
+                                        .padding(.top, -8)
+                                    Text("BOOK")
+                                        .font(.custom("Poppins-Regular", size: 13))
+                                        .foregroundStyle(.gold)
+                                        .padding(.top, -16)
+                                    Text("OF")
+                                        .font(.custom("Poppins-Regular", size: 13))
+                                        .foregroundStyle(.gold)
+                                        .padding(.top, -16)
+                                    Text("MORMON")
+                                        .font(.custom("Poppins-Regular", size: 13))
+                                        .foregroundStyle(.gold)
+                                        .padding(.top, -16)
+                                    
+                                    Rectangle()
+                                        .stroke(.gold, lineWidth: 1)
+                                        .frame(width: 80, height: 1)
+                                        .padding(.top, -10)
+                                }
+                                .padding(.top, -40)
+                            }
+                            .frame(width: 100, height: 150)
+                            .background(.bomBlue)
+                            .cornerRadius(5)
+                           
+                        }
+                        .background(.black)
+                        VStack {
+                            VStack {
+                                
+                                Rectangle()
+                                    .stroke(.gold, lineWidth: 1)
+                                    .frame(width: 80, height: 1)
+                                Text("PEARL OF")
+                                    .font(.custom("Poppins-Regular", size: 13))
+                                    .foregroundStyle(.gold)
+                                    .padding(.top, -8)
+                                Text("GREAT PRICE")
+                                    .font(.custom("Poppins-Regular", size: 13))
+                                    .foregroundStyle(.gold)
+                                    .padding(.top, -16)
+                                
+                                Rectangle()
+                                    .stroke(.gold, lineWidth: 1)
+                                    .frame(width: 80, height: 1)
+                                    .padding(.top, -10)
+                            }
+                            .padding(.top, -55)
+                        }
+                        .background(.black)
+                        .frame(width: 100, height: 150)
+                        .overlay(
+                             RoundedRectangle(cornerRadius: 5)
+                                 .stroke(Color.gray, lineWidth: 1) // 2. Apply border to same shape
+                         )
+                        .cornerRadius(5)
+                        VStack {
+                            VStack {
+                                
+                                Rectangle()
+                                    .stroke(.gold, lineWidth: 1)
+                                    .frame(width: 80, height: 1)
+                                Text("DOCTRINE")
+                                    .font(.custom("Poppins-Regular", size: 13))
+                                    .foregroundStyle(.gold)
+                                    .padding(.top, -8)
+                                Text("AND")
+                                    .font(.custom("Poppins-Regular", size: 13))
+                                    .foregroundStyle(.gold)
+                                    .padding(.top, -16)
+                                Text("COVENANTS")
+                                    .font(.custom("Poppins-Regular", size: 13))
+                                    .foregroundStyle(.gold)
+                                    .padding(.top, -16)
+                                
+                                Rectangle()
+                                    .stroke(.gold, lineWidth: 1)
+                                    .frame(width: 80, height: 1)
+                                    .padding(.top, -10)
+                            }
+                            .padding(.top, -53)
+                        }
+                        .frame(width: 100, height: 150)
+                        .background(.black)
+                        .overlay(
+                             RoundedRectangle(cornerRadius: 5)
+                                 .stroke(Color.gray, lineWidth: 1) // 2. Apply border to same shape
+                         )
+                        .cornerRadius(5)
+                        .cornerRadius(5)
+                        
+                    }
+                    .background(.black)
+                    .padding(.top, 50)
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.black)
         }
     }
 }
---
+++ DualScripture/Models/Book.swift
@@ -0,0 +1,14 @@
+//
+//  Book.swift
+//  DualScripture
+//
+//  Created by Michael Knight on 3/7/26.
+//
+
+import Foundation
+
+struct Book  {
+    let name: String
+    let chapterCount: Int
+}
+
